6025b276652075b8601095875939b8121ac96c82
Flo <flofranchomme@gmail.com>
2021-12-12 17:27:57 +0100

    Communication of two clients via the server.

--- src/ClientThread.java
+++ src/ClientThread.java
@@ -1,20 +1,45 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientThread {
 
     private Socket socket;
     public ClientThread(Socket socket){
         this.socket = socket;
-        // here client doesn't stay connected, should add a loop
-        OutputStream output = null;//to send the data to the client (low level)
+
+        OutputStream output = null;
         try {
-            output = socket.getOutputStream();
+            output = socket.getOutputStream(); //to send the data to the client (low level, bytes)
             PrintWriter out = new PrintWriter(output, true);// wrap it in a PrintWriter to send data in text format
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out.println("Hello");
-            String response = in.readLine();
-            System.out.println(response);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//to receive the data from the server
+
+            System.out.println("Please enter your username");
+            Scanner userName = new Scanner(System.in);
+            String userNameClient = userName.nextLine();
+            out.println(userNameClient);// send the userName to the server
+
+            System.out.println("Do you want to send a message?");
+            if ((new Scanner(System.in)).nextLine().equals("yes")){
+                System.out.println("Please enter the recipient name of the message");
+                Scanner recipientName = new Scanner(System.in);
+                String recipientNameClient = recipientName.nextLine();
+                out.println(recipientNameClient);// send the recipient name to the server
+
+                System.out.println("Please enter the message");
+                Scanner message = new Scanner(System.in);
+                String messageToSend = message.nextLine();
+                out.println(messageToSend);// send the message to the server
+            }else{
+                System.out.println("Waiting for response");
+
+            }
+
+            while (true) { //to stay connected, should add a condition
+                String response = in.readLine();//reads the server response
+                System.out.println(response);
+
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/ServerThread.java
+++ src/ServerThread.java
@@ -1,24 +1,42 @@
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ServerThread extends Thread{
-    private Socket socket;
+    private Socket socketSender;
+    private HashMap<String, Socket> clientList;
 
-    public ServerThread(Socket socket){
-        this.socket = socket;
+    public ServerThread(Socket socket, HashMap<String,Socket> clientList){
+        this.socketSender = socket;
+        this.clientList = clientList;
     }
     @Override
     public void run() {
         try {
-            InputStream input = socket.getInputStream(); //to read data (at low level, bytes) sent from the client
+            InputStream input = socketSender.getInputStream(); //to read data (at low level, bytes) sent from the client
             BufferedReader reader = new BufferedReader(new InputStreamReader(input)); //wrap the InputStream in a BufferedReader to read data as String
-            String line = reader.readLine();    // reads a line of text
-            System.out.println(line);
-            OutputStream output = socket.getOutputStream();//to send the data to the client (low level)
-            PrintWriter writer = new PrintWriter(output, true);// wrap it in a PrintWriter to send data in text format
-            writer.println("Hello too");
-            socket.close();
+            String userName = reader.readLine(); //reads the userName sent by the client
+
+            System.out.println("name of the client: " + userName);
+            clientList.put(userName, socketSender);//adds the userName and the corresponding socket to the clientList
+
+            while (true) {//to stay connected, should add a condition
+                String recipient = reader.readLine();// reads the recipient name sent by the client
+                if(clientList.containsKey(recipient)) {
+                    Socket socketRecipient = clientList.get(recipient);
+                    OutputStream outputRecipient = socketRecipient.getOutputStream();//to send the data to the client (low level)
+                    PrintWriter writerRecipient = new PrintWriter(outputRecipient, true);// wrap it in a PrintWriter to send data in text format
+                    String message = reader.readLine();
+                    writerRecipient.println(message);
+                } else{
+                    OutputStream outputSender = socketSender.getOutputStream();//to send the data to the client (low level)
+                    PrintWriter writerSender = new PrintWriter(outputSender, true);// wrap it in a PrintWriter to send data in text format
+                    writerSender.println("The recipient is not connected");
+                    
+                }
+
+            }
+
 
         } catch (IOException e) {
             e.printStackTrace();

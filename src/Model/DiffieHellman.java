e101fe35990e9d1389c6628e918630d9809a9aea
mthonar <maxime.thonar@gmail.com>
2021-12-16 12:37:27 +0100

    refactor

--- src/Model/DiffieHellman.java
+++ src/Model/DiffieHellman.java
@@ -1,3 +1,5 @@
+package Model;
+
 import java.math.BigInteger;
 
 public class DiffieHellman {
--- src/Model/Server.java
+++ src/Model/Server.java
@@ -1,3 +1,5 @@
+package Model;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,7 +15,7 @@ public class Server {
         HashMap<String, Socket> clientList = new HashMap<>(); //HashMap contains the userNames (key) with the corresponding socket (value)
         try {
             serverSocket = new ServerSocket(portNumber); //creates the socket of the server
-            System.out.println("Server started");
+            System.out.println("Model.Server started");
         }catch (IOException e) {
             System.out.println("Unable to request port");
         }
@@ -21,7 +23,7 @@ public class Server {
             while(true){
                 System.out.println("Waiting for a new client");
                 Socket socket = serverSocket.accept(); //creates a socket for each new client accepted
-                System.out.println("Client accepted");
+                System.out.println("Model.Client accepted");
                 ServerThread serverThread = new ServerThread(socket, clientList); //creates the thread for the new client
                 serverThread.start();
                 threadArrayList.add(serverThread);

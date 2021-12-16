403ada7e4e80a064d3888d365f3b1d354754a85c
Flo <flofranchomme@gmail.com>
2021-12-16 16:52:12 +0100

    new client and new thread Server (don't work)

---
+++ src/Model/NewClient.java
@@ -0,0 +1,59 @@
+package Model;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class NewClient {
+
+    public static void main(String[] args) {
+        int portNumber = 666;
+        try {
+            Socket socket = new Socket("localhost", portNumber);
+            OutputStream output = null;
+            try {
+                output = socket.getOutputStream(); //to send the data to the client (low level, bytes)
+                PrintWriter out = new PrintWriter(output, true);// wrap it in a PrintWriter to send data in text format
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//to receive the data from the server
+                // loop
+                while (true) {
+                    listenToServer(in);
+                    String command = readConsole();
+                    sendToServer();
+                    // listenners should call sendToServer
+
+
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        catch (IOException e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
+
+    private static void listenToServer(BufferedReader in) {
+        String resp = null;//reads the server response
+        try {
+            resp = in.readLine();
+            System.out.println(resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(resp);
+    }
+
+    public static void sendToServer(PrintWriter out, int header, String message){
+        String messageToSend = header + message.nextLine();
+        out.println(messageToSend);
+    }
+
+    public static String readConsole(){
+        Scanner command = new Scanner(System.in);
+        String commandUser = command.nextLine();
+        return commandUser;
+    }
+}
---
+++ src/Model/ServerThreadNew.java
@@ -0,0 +1,58 @@
+package Model;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class ServerThreadNew extends Thread{
+    private Socket socketSender;
+    private HashMap<String, Socket> clientList;
+
+    public ServerThreadNew(Socket socket, HashMap<String,Socket> clientList){
+        this.socketSender = socket;
+        this.clientList = clientList;
+    }
+    @Override
+    public void run() {
+        try {
+            //done once
+            InputStream input = socketSender.getInputStream(); //to read data (at low level, bytes) sent from the client
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input)); //wrap the InputStream in a BufferedReader to read data as String
+            String data = reader.readLine(); //reads the userName sent by the client
+            
+
+            System.out.println("name of the client: " + userName);
+            clientList.put(userName, socketSender);//adds the userName and the corresponding socket to the clientList
+            String recipient = reader.readLine();// reads the recipient name sent by the client
+            if(clientList.containsKey(recipient)) { //if client connected
+                Socket socketRecipient = clientList.get(recipient);
+                String message = reader.readLine();
+                sendToClient(socketRecipient,message);
+                while (true) {//to stay connected, should add a condition
+                    String data = reader.readLine();
+                    System.out.println(data);
+                    sendToClient(socketRecipient,data);
+                }
+
+            } else{
+                sendToClient(socketSender, "The recipient is not connected" );
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void sendToClient(Socket socket, String message ){
+        OutputStream output = null;//to send the data to the client (low level)
+        try {
+            output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);// wrap it in a PrintWriter to send data in text format
+            writer.println(message);
+            output.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
